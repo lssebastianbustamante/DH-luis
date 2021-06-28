602a147713959dc6fd4bec935feea791fcd0d198
https://github.com/lssebastianbustamante <luissebastianbustamante@gmail.com>
2021-06-28 19:54:00 -0300

    rutas

--- app.js
+++ app.js
@@ -2,30 +2,14 @@ const express = require ("express");
 const { join } = require("path");
 const app = express ();
 const path = require ("path");
+const publicPath= path.resolve(__dirname, 'public');
+
+app.use(express.static(publicPath));
 
 app.get("/", (req, res ) => {
     res.sendFile(path.join(__dirname, "/views/index.html"));
 })
 
-app.get("/babbage", (req,res)=> {
-    res.sendFile(path.join(__dirname, "/views/babage.html"));
-})
-app.get ("/berners", (req,res)=>{
-    res.sendFile(path.join(__dirname,"/views/berners.html"));
-})
-app.get ("/clarke", (req,res)=> {
-     res.sendFile(path.join(__dirname,"/views/clarke.html"));
-})
-app.get ("/hamilton",(req,res)=> {
-    res.sendFile(path.join(__dirname,"/views/hamilton.html"));
-})
-app.get ("/hopper", (req,res)=> {
-    res.sendFile(path.join(__dirname,"/views/hopper.html"));
-})
-
-app.get("/turing", (req,res)=> {
-    res.sendFile(path.join(__dirname,"/views/turing.html"));
-})
 
 app.listen ("3030",()=>{
     console.log("Servidor funcionando en el puerto 3030");
